5d2fe32204df1c10f2f45739374cd751331aaf00
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-07-22 14:19:32 +0100

    added css to highlight wrong and correct answer with different colours and fixed back to game button and leaderboard
    setTimeout function is used to delay the redirect to the leaderboard page. This allows the user to see their results before being redirected.

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,19 +1,29 @@
-// Load leaderboard from local storage
-let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-
-// Sort the leaderboard by score
-leaderboard.sort((a, b) => b.score - a.score);
-
-// Display leaderboard
+// Get the leaderboard list and back button
 const leaderboardList = document.getElementById('leaderboard-list');
-leaderboardList.innerHTML = ''; // Clear the leaderboard
-leaderboard.forEach(entry => {
-    const listItem = document.createElement('li');
-    listItem.innerText = `${entry.name}: ${entry.score} points (${new Date(entry.date).toLocaleDateString()})`;
-    leaderboardList.appendChild(listItem);
-});
-// Add event listener to Back to Game button
-const backToGameButton = document.getElementById('back-to-game');
-backToGameButton.addEventListener('click', function() {
-    window.location.href = "index.html";  // assuming your game page is "index.html"
-});
+const backButton = document.getElementById('back-to-game');
+
+// Display leaderboard immediately when the page loads
+displayLeaderboard();
+
+// Set up event listener for back button
+backButton.addEventListener('click', backToGame);
+
+function displayLeaderboard() {
+  leaderboardList.innerHTML = ''; // Clear the leaderboard list
+
+  // Get the leaderboard from localStorage or initialize as an empty array if not found
+  const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
+  leaderboard.sort((a, b) => b.score - a.score); // Sort scores in descending order
+  
+  // Append each leaderboard entry to the leaderboard list
+  leaderboard.forEach((entry, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${index + 1}. ${entry.name} - ${entry.score}`;
+    leaderboardList.appendChild(li);
+  });
+}
+
+function backToGame() {
+  // Redirect to the game page
+  window.location.href = 'index.html';
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -20,149 +20,186 @@ const questionsElement = document.getElementById('questions');
 const startButton = document.getElementById('start-btn');
 
 fetch('https://opentdb.com/api_category.php')
-    .then(response => response.json())
-    .then(data => {
-        data.trivia_categories.forEach(category => {
-            const option = document.createElement('option');
-            option.value = category.id;
-            option.text = category.name;
-            categoryElement.appendChild(option);
-        });
+  .then(response => response.json())
+  .then(data => {
+    data.trivia_categories.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category.id;
+      option.text = category.name;
+      categoryElement.appendChild(option);
     });
+  });
 
 startButton.addEventListener('click', () => {
-    fetch(`https://opentdb.com/api.php?amount=${questionsElement.value}&category=${categoryElement.value}&difficulty=${difficultyElement.value}&type=multiple`)
-        .then(response => response.json())
-        .then(data => {
-            questions = data.results;
-            startQuiz();
-        });
+  fetch(`https://opentdb.com/api.php?amount=${questionsElement.value}&category=${categoryElement.value}&difficulty=${difficultyElement.value}&type=multiple`)
+    .then(response => response.json())
+    .then(data => {
+      questions = data.results;
+      startQuiz();
+    });
 });
 
 function startQuiz() {
-    score = 0;
-    currentQuestionIndex = 0;
-    startButton.hidden = true;
-    nextButton.hidden = false;
-    quitButton.hidden = false;
-    questionContainerElement.hidden = false;
-    showQuestion();
+  score = 0;
+  currentQuestionIndex = 0;
+  startButton.hidden = true;
+  nextButton.hidden = false;
+  quitButton.hidden = false;
+  questionContainerElement.hidden = false;
+  showQuestion();
 }
 
 function showQuestion() {
-    startTimer();
-    questionElement.innerText = decodeHTML(questions[currentQuestionIndex].question);
-    const choices = [...questions[currentQuestionIndex].incorrect_answers, questions[currentQuestionIndex].correct_answer];
-    choices.sort(() => Math.random() - 0.5);
-    choiceContainerElement.innerHTML = '';
-    choices.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = decodeHTML(answer);
-        button.classList.add('choice');
-        button.addEventListener('click', selectAnswer);
-        choiceContainerElement.appendChild(button);
-    });
-    questionsLeftElement.innerText = `Questions left: ${questions.length - currentQuestionIndex}`;
+  startTimer();
+
+  questionElement.innerText = decodeHTML(questions[currentQuestionIndex].question);
+
+  choiceContainerElement.innerHTML = '';
+  const choices = [...questions[currentQuestionIndex].incorrect_answers, questions[currentQuestionIndex].correct_answer];
+  choices.sort(() => Math.random() - 0.5);
+  choices.forEach(choice => {
+    const button = document.createElement('button');
+    button.innerText = decodeHTML(choice);
+    button.classList.add('choice');
+    button.addEventListener('click', selectAnswer);
+    choiceContainerElement.appendChild(button);
+  });
+
+  questionsLeftElement.innerText = `Questions left: ${questions.length - currentQuestionIndex}`;
 }
 
 function selectAnswer(e) {
-    clearInterval(timer);
-    const selectedButton = e.target;
-    const correctAnswer = questions[currentQuestionIndex].correct_answer;
-    if (selectedButton.innerText === correctAnswer) {
-        selectedButton.classList.add('correct');
-        score++;
-    } else {
-        selectedButton.classList.add('wrong');
-    }
+  clearInterval(timer);
+
+  const selectedButton = e.target;
+  const correctAnswer = questions[currentQuestionIndex].correct_answer;
+
+  // Remove the event listener to prevent multiple selections
+  selectedButton.removeEventListener('click', selectAnswer);
+
+  if (selectedButton.innerText === correctAnswer) {
+    selectedButton.classList.add('correct');
+    score++;
+  } else {
+    selectedButton.classList.add('wrong');
+    // Highlight the correct answer
     Array.from(choiceContainerElement.children).forEach(button => {
-        button.disabled = true;
+      if (button.innerText === correctAnswer) {
+        button.classList.add('correct');
+      }
     });
-    nextButton.disabled = false;
-    scoreElement.innerText = score;
+  }
+
+  nextButton.disabled = false;
+  scoreElement.innerText = score;
 }
 
 nextButton.addEventListener('click', () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        nextButton.disabled = true;
-        showQuestion();
-    } else {
-        endQuiz();
-    }
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    nextButton.disabled = true;
+    showQuestion();
+  } else {
+    endQuiz();
+  }
 });
 
 quitButton.addEventListener('click', () => {
-    let confirmQuit = confirm('Are you sure you want to quit the quiz?');
-    if (confirmQuit) {
-        clearInterval(timer);
-        questionContainerElement.hidden = true;
-        startButton.hidden = false;
-        nextButton.hidden = true;
-        quitButton.hidden = true;
-        scoreElement.innerText = '';
+    let isConfirmed = confirm('Are you sure you want to exit the game?');
+    if(isConfirmed) {
+      window.location.href = 'index.html';
     }
-});
+  });
+  
 
 function endQuiz() {
-    const results = document.createElement('div');
-    const percentageScore = (score / questionsElement.value) * 100;
+  const userName = prompt('Enter your name for the leaderboard:');
+  
+  let scores = JSON.parse(localStorage.getItem('scores')) || [];
+  
+  scores.push({ name: userName, score: score });
+  localStorage.setItem('scores', JSON.stringify(scores));
+  
+  const results = document.createElement('div');
+  const percentageScore = (score / questionsElement.value) * 100;
 
-    let message;
-    if (percentageScore === 100) {
-        message = "Incredible! You've answered every question correctly.";
-    } else if (percentageScore >= 75) {
-        message = "Well done! You've scored highly.";
-    } else if (percentageScore >= 50) {
-        message = "Good job! You've got more than half the answers right.";
-    } else if (percentageScore >= 25) {
-        message = "Don't worry, keep practicing!";
-    } else {
-        message = "Tough quiz, better luck next time!";
-    }
+  let message;
+  if (percentageScore === 100) {
+    message = "Incredible! You've answered every question correctly.";
+  } else if (percentageScore >= 75) {
+    message = "Well done! You've scored highly.";
+  } else if (percentageScore >= 50) {
+    message = "Good job! You've got more than half the answers right.";
+  } else if (percentageScore >= 25) {
+    message = "Don't worry, keep practicing!";
+  } else {
+    message = "Tough quiz, better luck next time!";
+  }
 
-    results.innerHTML = `
-        <h2>You finished the quiz!</h2>
-        <p>Your score: ${score} out of ${questionsElement.value}</p>
-        <p>${percentageScore.toFixed(2)}% correct</p>
-        <p>${message}</p>
-    `;
+  results.innerHTML = `
+    <h2>You finished the quiz!</h2>
+    <p>Your score: ${score} out of ${questionsElement.value}</p>
+    <p>${percentageScore.toFixed(2)}% correct</p>
+    <p>${message}</p>
+  `;
 
-    questionContainerElement.innerHTML = '';
-    questionContainerElement.appendChild(results);
-    startButton.innerText = 'Restart Quiz';
-    startButton.hidden = false;
+  questionContainerElement.innerHTML = '';
+  questionContainerElement.appendChild(results);
 
-    startButton.addEventListener('click', function() {
-        location.reload();
-    });
+  startButton.innerText = 'Restart Quiz';
+  startButton.hidden = false;
+
+  startButton.addEventListener('click', function() {
+    location.reload();
+  });
+
+  setTimeout(() => {
+    window.location.href = 'leaderboard.html';
+  }, 5000);
 }
 
 function startTimer() {
     let timeLeft = parseInt(timerElement.value);
     timerCountdownElement.innerText = `Time left: ${timeLeft}s`;
+  
     progressBar.style.width = '100%';
+  
     timer = setInterval(() => {
-        timeLeft--;
-        timerCountdownElement.innerText = `Time left: ${timeLeft}s`;
-        progressBar.style.width = `${(timeLeft / parseInt(timerElement.value)) * 100}%`;
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            timeUp();
-        }
+      timeLeft--;
+      timerCountdownElement.innerText = `Time left: ${timeLeft}s`;
+  
+      let percentageLeft = (timeLeft / parseInt(timerElement.value)) * 100;
+      progressBar.style.width = `${percentageLeft}%`;
+  
+      if (timeLeft <= 0) {
+        clearInterval(timer);
+        timeUp();
+      }
     }, 1000);
-}
+  }
+  
 
-function timeUp() {
+  function timeUp() {
     Array.from(choiceContainerElement.children).forEach(button => {
-        button.disabled = true;
+      button.disabled = true;
     });
-    nextButton.disabled = false;
+  
     timerCountdownElement.innerText = 'Time is up!';
-}
+  
+    setTimeout(() => {
+      currentQuestionIndex++;
+      if (currentQuestionIndex < questions.length) {
+        showQuestion();
+      } else {
+        endQuiz();
+      }
+    }, 2000);  // this will wait for 2 seconds before moving to the next question
+  }
+  
 
-function decodeHTML(html) {
-    const txt = document.createElement('textarea');
-    txt.innerHTML = html;
-    return txt.value;
+function decodeHTML(encodedString) {
+  const textarea = document.createElement('textarea');
+  textarea.innerHTML = encodedString;
+  return textarea.value;
 }
