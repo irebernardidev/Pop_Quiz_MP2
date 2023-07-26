ed71bf18badad6221ccc72322d1ab34a0aaace0d
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-07-26 14:24:35 +0100

    added sound to quiz for correct and wrong answer and congrats music, also changed the progress bar colors from green to yellow and red when time's up, finally added description for each function in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,10 @@
+// Initialize score, currentQuestionIndex and questions as variables
 let score = 0;
 let currentQuestionIndex = 0;
 let questions = [];
 let timer;
 
+// Define HTML elements
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const choiceContainerElement = document.getElementById('choice-container');
@@ -12,13 +14,17 @@ const quitButton = document.getElementById('quit-btn');
 const timerCountdownElement = document.getElementById('timer-countdown');
 const progressBar = document.getElementById('progress-bar');
 const questionsLeftElement = document.getElementById('questions-left');
-
 const categoryElement = document.getElementById('category');
 const difficultyElement = document.getElementById('difficulty');
 const timerElement = document.getElementById('timer');
 const questionsElement = document.getElementById('questions');
 const startButton = document.getElementById('start-btn');
+const correctSound = document.getElementById('correct-sound');
+const wrongSound = document.getElementById('wrong-sound');
+const congratsMusic = document.getElementById('congrats-music');
 
+
+// Fetch and populate categories from API
 fetch('https://opentdb.com/api_category.php')
   .then(response => response.json())
   .then(data => {
@@ -30,15 +36,18 @@ fetch('https://opentdb.com/api_category.php')
     });
   });
 
+// Event listener for start button click
 startButton.addEventListener('click', () => {
+  // Fetch quiz questions from API based on user's choices of category, difficulty and number of questions
   fetch(`https://opentdb.com/api.php?amount=${questionsElement.value}&category=${categoryElement.value}&difficulty=${difficultyElement.value}&type=multiple`)
     .then(response => response.json())
     .then(data => {
       questions = data.results;
-      startQuiz();
+      startQuiz(); // Start the quiz
     });
 });
 
+// Function to start the quiz
 function startQuiz() {
   score = 0;
   currentQuestionIndex = 0;
@@ -46,14 +55,17 @@ function startQuiz() {
   nextButton.hidden = false;
   quitButton.hidden = false;
   questionContainerElement.hidden = false;
-  showQuestion();
+  showQuestion(); // Show the first question
 }
 
+// Function to show a question
 function showQuestion() {
-  startTimer();
+  startTimer(); // Start the timer
 
+  // Display the question
   questionElement.innerText = decodeHTML(questions[currentQuestionIndex].question);
 
+  // Clear previous question's choices and create new ones
   choiceContainerElement.innerHTML = '';
   const choices = [...questions[currentQuestionIndex].incorrect_answers, questions[currentQuestionIndex].correct_answer];
   choices.sort(() => Math.random() - 0.5);
@@ -65,43 +77,48 @@ function showQuestion() {
     choiceContainerElement.appendChild(button);
   });
 
+  // Display remaining questions count
   questionsLeftElement.innerText = `Questions left: ${questions.length - currentQuestionIndex}`;
 }
 
+// Function to handle the selection of an answer
 function selectAnswer(e) {
-  clearInterval(timer);
+  clearInterval(timer); // Stop the timer
 
   const selectedButton = e.target;
   const correctAnswer = questions[currentQuestionIndex].correct_answer;
 
-  // Remove the event listener to prevent multiple selections
+  // Disable all buttons and remove event listeners
   selectedButton.removeEventListener('click', selectAnswer);
-  
-  /// Disable all buttons once a selection has been made
   Array.from(choiceContainerElement.children).forEach(button => {
     button.disabled = true;
   });
   
+  // Update score and styles based on whether the answer was correct or incorrect
   if (selectedButton.innerText === correctAnswer) {
     selectedButton.classList.add('correct');
     score++;
+    correctSound.play();
   } else {
     selectedButton.classList.add('wrong');
-    // Highlight the correct answer
     Array.from(choiceContainerElement.children).forEach(button => {
       if (button.innerText === correctAnswer) {
         button.classList.add('correct');
       }
     });
+    wrongSound.play();
   }
 
+  // Enable the next button and update the score display
   nextButton.disabled = false;
   scoreElement.innerText = score;
 }
 
+// Event listener for next button click
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++;
 
+  // Check if there are more questions, if not end the quiz
   if (currentQuestionIndex < questions.length) {
     nextButton.disabled = true;
     showQuestion();
@@ -110,37 +127,43 @@ nextButton.addEventListener('click', () => {
   }
 });
 
+// Event listener for quit button click
 quitButton.addEventListener('click', () => {
   let isConfirmed = confirm('Are you sure you want to exit the game?');
   if (isConfirmed) {
-    window.location.href = 'index.html';
+    window.location.href = 'index.html'; // Navigate to the home page
   }
 });
 
+// Function to end the quiz
 function endQuiz() {
   const userName = prompt('Enter your name for the leaderboard:');
 
+  // Retrieve previous scores from local storage or initialize if it doesn't exist
   let scores = JSON.parse(localStorage.getItem('scores')) || [];
 
+  // Add current score to scores
   scores.push({
     name: userName,
     score: score,
-    time: getDateTime()
+    time: getDateTime() // Get current date and time
   });
+  // Store updated scores in local storage
   localStorage.setItem('scores', JSON.stringify(scores));
 
-
+// Show the quiz results
   const results = document.createElement('div');
-  const percentageScore = (score / questionsElement.value) * 100;
+  const percentageScore = (score / parseInt(questionsElement.value, 10)) * 100;
+
 
   let message;
   if (percentageScore === 100) {
     message = "Incredible! You've answered every question correctly.";
     const congratsElement = document.getElementById('congratulation');
-    const congratsMusic = document.getElementById('congrats-music');
-    congratsElement.style.display = 'block'; // Show the congratulation message
-    congratsElement.classList.add('congrats'); // Add the animation
-    congratsMusic.play(); // Play the music
+    congratsElement.style.display = 'block';
+    congratsElement.classList.add('congrats');
+    congratsMusic.play();
+  
   } else if (percentageScore >= 75) {
     message = "Well done! You've scored highly.";
   } else if (percentageScore >= 50) {
@@ -167,22 +190,20 @@ function endQuiz() {
   startButton.addEventListener('click', function () {
     location.reload();
   });
-
+// Redirect to leaderboard after 10 seconds
   setTimeout(() => {
     window.location.href = 'leaderboard.html';
-  }, 5000);
+  }, 10000);
 }
 
-function getDateTime() {
-  const now = new Date();
-  return `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
-}
-
+// Function to start a timer
 function startTimer() {
   let timeLeft = parseInt(timerElement.value);
   timerCountdownElement.innerText = `Time left: ${timeLeft}s`;
 
+// Change progress bar color based on remainig time percentage
   progressBar.style.width = '100%';
+  progressBar.style.backgroundColor = '#4CAF50'; // set color to green at the start
 
   timer = setInterval(() => {
     timeLeft--;
@@ -191,32 +212,37 @@ function startTimer() {
     let percentageLeft = (timeLeft / parseInt(timerElement.value)) * 100;
     progressBar.style.width = `${percentageLeft}%`;
 
+    if (percentageLeft <= 50 && percentageLeft > 20) {
+      progressBar.style.backgroundColor = '#FFDD00'; // set color to yellow if 50% < percentageLeft <= 20%
+    } else if (percentageLeft <= 20) {
+      progressBar.style.backgroundColor = '#FF5722'; // set color to red if percentageLeft <= 20%
+    }
+
     if (timeLeft <= 0) {
       clearInterval(timer);
       timeUp();
     }
   }, 1000);
 }
-
+// Function to handle when time is up
 function timeUp() {
   Array.from(choiceContainerElement.children).forEach(button => {
     button.disabled = true;
+    if (button.innerText === questions[currentQuestionIndex].correct_answer) {
+      button.classList.add('correct');
+    }
   });
 
-  timerCountdownElement.innerText = 'Time is up!';
-
-  setTimeout(() => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-      showQuestion();
-    } else {
-      endQuiz();
-    }
-  }, 2000); // this will wait for 2 seconds before moving to the next question
+  nextButton.disabled = false;
 }
 
-function decodeHTML(encodedString) {
-  const textarea = document.createElement('textarea');
-  textarea.innerHTML = encodedString;
-  return textarea.value;
+function getDateTime() {
+  const date = new Date();
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+}
+// Function to decode HTML entities
+function decodeHTML(html) {
+  var txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
 }
