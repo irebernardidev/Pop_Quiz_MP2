40200b0a121921bb2eea2c7b684a80869d138294
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-08-09 14:57:46 +0200

    dedd auto response to  contact form using emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -6,19 +6,40 @@
 emailjs.init('zn6LDYixJTe5o00_B');
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  // Send email via EmailJS
-  emailjs.sendForm('service_opj28eh', 'contact_form', this, 'zn6LDYixJTe5o00_B')
-    .then(function () {
-      console.log('Email successfully sent!');
-      alert('Email successfully sent!');
-      // Clear the input fields
-      document.getElementById('name').value = '';
-      document.getElementById('email').value = '';
-      document.getElementById('message').value = '';
-    }, function (error) {
-      console.log('There has been an error: ', error);
-      alert('There was an error sending your email.');
-    });
+    // Send email via EmailJS to the intended recipient
+    emailjs.sendForm('service_opj28eh', 'contact_form', this, 'zn6LDYixJTe5o00_B')
+        .then(() => {
+            console.log('Email successfully sent to recipient!');
+            // Auto-reply to the user
+            sendAutoReply();
+        }, (error) => {
+            console.log('There has been an error sending to recipient: ', error);
+            alert('There was an error sending your email.');
+        });
 });
+
+function sendAutoReply() {
+    const userEmail = document.getElementById('email').value;
+
+    // Create the auto-reply data based on the user's email
+    const autoReplyData = {
+        user_email: userEmail, 
+        subject: "Thank you for contacting us!",
+        message: "Hello! Thank you for reaching out to us. We've received your message and will get back to you shortly."
+    };
+
+    emailjs.send('service_opj28eh', 'contact_form', autoReplyData, 'zn6LDYixJTe5o00_B')
+        .then(() => {
+            console.log('Auto-reply successfully sent!');
+            alert('Email successfully sent!');
+            // Clear the input fields
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('message').value = '';
+        }, (error) => {
+            console.log('There has been an error sending auto-reply: ', error);
+            alert('There was an error sending the auto-reply email.');
+        });
+}
