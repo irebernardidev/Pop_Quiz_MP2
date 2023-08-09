6743e183c47769c63378fb3ddb38000a7713496c
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-08-09 17:36:42 +0200

    update to emailjs- double email bug fixed
    a double email was sent to user as auto-reply, bug was fixed by creating a new email template in emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,14 +1,12 @@
 /* global emailjs */
-
 // tutorial used [Medium- Jeffrey Martinez](https://martinezjf2.medium.com/how-to-setup-emailjs-33809350f0f8) - setting up EmailJS.
-
 // Initialize EmailJS with your user ID
 emailjs.init('zn6LDYixJTe5o00_B');
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Send email via EmailJS to the intended recipient
+    // Send the main contact message to the intended recipient
     emailjs.sendForm('service_opj28eh', 'contact_form', this, 'zn6LDYixJTe5o00_B')
         .then(() => {
             console.log('Email successfully sent to recipient!');
@@ -22,15 +20,18 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
 
 function sendAutoReply() {
     const userEmail = document.getElementById('email').value;
+    const userName = document.getElementById('name').value;
 
-    // Create the auto-reply data based on the user's email
+    // Create the auto-reply data including the user's name
     const autoReplyData = {
-        user_email: userEmail, 
+        user_email: userEmail,
+        user_name: userName,  
         subject: "Thank you for contacting us!",
-        message: "Hello! Thank you for reaching out to us. We've received your message and will get back to you shortly."
+        message: `Hello ${userName}! Thank you for reaching out to us. We've received your message and will get back to you shortly.`
     };
 
-    emailjs.send('service_opj28eh', 'contact_form', autoReplyData, 'zn6LDYixJTe5o00_B')
+    // Use the 'auto_reply_template' for sending the auto-reply
+    emailjs.send('service_opj28eh', 'template_jmmhph8', autoReplyData, 'zn6LDYixJTe5o00_B')
         .then(() => {
             console.log('Auto-reply successfully sent!');
             alert('Email successfully sent!');
